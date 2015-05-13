8d01d4a54b31828891d9b1cc93e97272afde3ab8
fabio-silva <ffjs1993@gmail.com>
2015-05-13 19:25:05 +0100

    Duties

--- src/com/elements/Flight.java
+++ src/com/elements/Flight.java
@@ -9,20 +9,77 @@ public class Flight {
 
     private String origin;
     private String destination;
-    Date departureTime;
-    Date arrivalTime;
-    ArrayList<Integer> operatingDays; //Binary array
+    private String equipment;
+    private Date departureTime;
+    private Date arrivalTime;
+    private Integer dayOfMonth;
+    private ArrayList<Integer> operatingDays; //Binary array
 
-    public Flight(String origin, String destination, String departureTime, String arrivalTime, String days){
+    public Flight(String origin, String destination, String equipment, String departureTime, String arrivalTime, String days){
         this.origin = origin;
         this.destination = destination;
+        this.equipment = equipment;
         this.operatingDays = parseDays(days);
-
+        this.dayOfMonth = 0;
         try{
             this.arrivalTime = new SimpleDateFormat("HH:mm").parse(arrivalTime);
             this.departureTime = new SimpleDateFormat("HH:mm").parse(departureTime);
         }catch (ParseException ex){
             System.out.println("Error parsing date");
+        }
+    }
+
+    public Flight(Flight f){
+        this.origin = f.origin;
+        this.destination = f.destination;
+        this.equipment = f.equipment;
+        this.operatingDays = f.operatingDays;
+        this.arrivalTime = f.arrivalTime;
+        this.departureTime = f.departureTime;
+        this.dayOfMonth = f.dayOfMonth;
+    }
+
+    public String getOrigin(){
+        return origin;
+    }
+
+    public String getDestination(){
+        return destination;
+    }
+
+    public String getEquipment(){
+        return equipment;
+    }
+
+    public Date getDepartureTime(){
+        return departureTime;
+    }
+
+    public Date getArrivalTime(){
+        return arrivalTime;
+    }
+
+    public ArrayList<Integer> getOperatingDays(){
+        return operatingDays;
+    }
+
+    public Integer getDayOfMonth(){
+        return dayOfMonth;
+    }
+
+    public void setDayOfMonth(Integer day){
+        this.dayOfMonth = day;
+        String originalDeparture = new SimpleDateFormat("HH:mm").format(departureTime);
+        String originalArrival = new SimpleDateFormat("HH:mm").format(arrivalTime);
+        String newDeparture = "2014-05-" + day + " " + originalDeparture;
+        String newArrival = "2014-05-" + day + " " + originalArrival;
+
+        try {
+            departureTime = new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(newDeparture);
+            arrivalTime = new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(newArrival);
+            System.out.println("NEW DATE = " + departureTime);
+        }catch (ParseException ex){
+            System.out.println("Parse Error");
         }
     }
 
--- src/com/project/Main.java
+++ src/com/project/Main.java
@@ -1,14 +1,81 @@
 package com.project;
 
+import com.elements.Flight;
 import com.simplex.Simplex;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Map<Integer, ArrayList<Flight> > flights = new HashMap<Integer, ArrayList<Flight>>();
+        ArrayList< Flight > fileFlights = new ArrayList<Flight>();
+
+        for(int i = 1; i <= 28; i++){
+            flights.put(i, new ArrayList<Flight>());
+        }
+
+        try {
+            String line;
+            BufferedReader reader = new BufferedReader(new FileReader("flightSchedule"));
+            while((line = reader.readLine()) != null){
+                String[] parts = line.split(" ");
+                fileFlights.add(new Flight(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]));
+
+            }
+        }catch(FileNotFoundException ex){
+            System.out.println("Not such file");
+        }catch(IOException ex){
+            System.out.println("Error reading");
+        }
+
+        for(int i = 0; i < 7; i++){
+            ArrayList<Flight> firstWeekFlights = flights.get(i + 1);
+            ArrayList<Flight> secondWeekFlights = flights.get((i + 1) + 7);
+            ArrayList<Flight> thirdWeekFlights = flights.get((i + 1) + 14);
+            ArrayList<Flight> fourthWeekFlights = flights.get((i + 1) + 21);
+
+            for(int k = 0; k < fileFlights.size(); k++){
+                if(fileFlights.get(k).getOperatingDays().get(i) == 1){
+                    firstWeekFlights.add(new Flight(fileFlights.get(k)));
+                    secondWeekFlights.add(new Flight(fileFlights.get(k)));
+                    thirdWeekFlights.add(new Flight(fileFlights.get(k)));
+                    fourthWeekFlights.add(new Flight(fileFlights.get(k)));
+                }
+            }
+
+            for(int k = 0; k < firstWeekFlights.size(); k++){
+                firstWeekFlights.get(k).setDayOfMonth(i + 1);
+            }
+            for(int k = 0; k < secondWeekFlights.size(); k++){
+                secondWeekFlights.get(k).setDayOfMonth((i + 1) + 7);
+            }
+            for(int k = 0; k < thirdWeekFlights.size(); k++){
+                thirdWeekFlights.get(k).setDayOfMonth((i + 1) + 14);
+            }
+            for(int k = 0; k < fourthWeekFlights.size(); k++){
+                fourthWeekFlights.get(k).setDayOfMonth((i + 1) + 21);
+            }
+
+
+
+        }
+
+       /* for(int i = 1; i <= 28; i++){
+            System.out.println("------------------DAY " + i + "------------------------");
+            for(int j = 0; j < flights.get(i).size(); j++){
+                System.out.println(flights.get(i).get(j).getOrigin() + " -> " + flights.get(i).get(j).getDestination() + ", DAY = " + flights.get(i).get(j).getDayOfMonth());
+            }
+        }*/
 
 
         ArrayList<Double> costMatrix = new ArrayList<Double>(Arrays.asList(30.0, 40.0, 50.0));
@@ -20,7 +87,7 @@ public class Main {
         ));
 
 
-	    Simplex exampleProblem = new Simplex(costMatrix, problemMatrix);
+        Simplex exampleProblem = new Simplex(costMatrix, problemMatrix);
 
         exampleProblem.solve();
 
