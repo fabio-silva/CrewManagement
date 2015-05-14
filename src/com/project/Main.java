21087ac8238bcf8f8fa78b255af239b6807a1e86
emanuelpinho <emanuelaspinho@gmail.com>
2015-05-14 23:12:44 +0100

    Pairings generation done and apply to simplex. Add a second file minify to fast tests

--- src/com/project/Main.java
+++ src/com/project/Main.java
@@ -2,6 +2,7 @@ package com.project;
 
 import com.elements.Duty;
 import com.elements.Flight;
+import com.elements.Pairing;
 import com.simplex.Simplex;
 
 import java.io.BufferedReader;
@@ -23,17 +24,16 @@ public class Main {
         Map<Integer, ArrayList<Flight> > flights = new HashMap<Integer, ArrayList<Flight>>();
         ArrayList< Flight > fileFlights = new ArrayList<Flight>();
 
-        for(int i = 1; i <= 28; i++){
+        for(int i = 1; i <= 7; i++){  // CHAGE TO 28
             flights.put(i, new ArrayList<Flight>());
         }
 
         try {
             String line;
-            BufferedReader reader = new BufferedReader(new FileReader("flightSchedule"));
+            BufferedReader reader = new BufferedReader(new FileReader("flightSchedule_min"));
             while((line = reader.readLine()) != null){
                 String[] parts = line.split(" ");
                 fileFlights.add(new Flight(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]));
-
             }
         }catch(FileNotFoundException ex){
             System.out.println("Not such file");
@@ -41,36 +41,38 @@ public class Main {
             System.out.println("Error reading");
         }
 
+        int flightId = 0;
         for(int i = 0; i < 7; i++){
             ArrayList<Flight> firstWeekFlights = flights.get(i + 1);
-            ArrayList<Flight> secondWeekFlights = flights.get((i + 1) + 7);
-            ArrayList<Flight> thirdWeekFlights = flights.get((i + 1) + 14);
-            ArrayList<Flight> fourthWeekFlights = flights.get((i + 1) + 21);
+            //ArrayList<Flight> secondWeekFlights = flights.get((i + 1) + 7);
+            //ArrayList<Flight> thirdWeekFlights = flights.get((i + 1) + 14);
+            //ArrayList<Flight> fourthWeekFlights = flights.get((i + 1) + 21);
 
             for(int k = 0; k < fileFlights.size(); k++){
                 if(fileFlights.get(k).getOperatingDays().get(i) == 1){
                     firstWeekFlights.add(new Flight(fileFlights.get(k)));
-                    secondWeekFlights.add(new Flight(fileFlights.get(k)));
-                    thirdWeekFlights.add(new Flight(fileFlights.get(k)));
-                    fourthWeekFlights.add(new Flight(fileFlights.get(k)));
+                    //secondWeekFlights.add(new Flight(fileFlights.get(k)));
+                    //thirdWeekFlights.add(new Flight(fileFlights.get(k)));
+                    //fourthWeekFlights.add(new Flight(fileFlights.get(k)));
                 }
             }
 
             for(int k = 0; k < firstWeekFlights.size(); k++){
-                firstWeekFlights.get(k).setDayOfMonth(i + 1);
+                firstWeekFlights.get(k).setDayOfMonth(i + 1, flightId);
+                flightId++;
             }
-            for(int k = 0; k < secondWeekFlights.size(); k++){
-                secondWeekFlights.get(k).setDayOfMonth((i + 1) + 7);
+            /*for(int k = 0; k < secondWeekFlights.size(); k++){
+                secondWeekFlights.get(k).setDayOfMonth((i + 1) + 7, flightId);
+                flightId++;
             }
             for(int k = 0; k < thirdWeekFlights.size(); k++){
-                thirdWeekFlights.get(k).setDayOfMonth((i + 1) + 14);
+                thirdWeekFlights.get(k).setDayOfMonth((i + 1) + 14, flightId);
+                flightId++;
             }
             for(int k = 0; k < fourthWeekFlights.size(); k++){
-                fourthWeekFlights.get(k).setDayOfMonth((i + 1) + 21);
-            }
-
-
-
+                fourthWeekFlights.get(k).setDayOfMonth((i + 1) + 21, flightId);
+                flightId++;
+            }*/
         }
 
       /*  for(int i = 1; i <= 28; i++){
@@ -81,15 +83,59 @@ public class Main {
         }*/
 
         HashSet<Duty> duties = Duty.makeDuties(flights);
-        int l = 0;
+
+        System.out.println("Number of duties: " + duties.size());
+
         for(Duty d : duties){
-            System.out.println("-------------DUTY----------------");
-            for(int j = 0; j < d.getFlights().size(); j++){
-                System.out.println(d.getFlights().get(j).getOrigin() + "->" + d.getFlights().get(j).getDestination()
-                + ", " + d.getFlights().get(j).getDepartureTime() + "->" + d.getFlights().get(j).getArrivalTime());
+            if(hasFlight(d)) {
+                System.out.println("-------------DUTY----------------");
+                for (int j = 0; j < d.getFlights().size(); j++) {
+                    System.out.println(d.getFlights().get(j).getOrigin() + "->" + d.getFlights().get(j).getDestination()
+                            + ", " + d.getFlights().get(j).getDepartureTime() + "->" + d.getFlights().get(j).getArrivalTime());
+                }
             }
         }
 
+        HashSet<Pairing> pairings = Pairing.makePairings(duties);
+
+        ArrayList<Pairing> pairingsList = new ArrayList<Pairing>(pairings);
+
+        System.out.println("Number of pairings after remove feasible: " + pairingsList.size());
+
+        ArrayList<ArrayList<Double>> matrix = new ArrayList<ArrayList<Double>>();
+        ArrayList<Double> flightLine;
+        ArrayList<Double> costMatrix = new ArrayList<Double>();
+
+
+        for(Map.Entry<Integer, ArrayList<Flight>> f : flights.entrySet()){
+            System.out.println("\n\nNew Day");
+            for(int i = 0; i < f.getValue().size(); i++){
+                System.out.println("Flight :" + f.getValue().get(i).getFlightId());
+
+                flightLine = new ArrayList<Double>();
+                for(Pairing p : pairingsList){
+                    if (p.hasFlight(f.getValue().get(i).getFlightId())){
+                        flightLine.add(1.0);
+                    }
+                    else {
+                       flightLine.add(0.0);
+                    }
+                }
+                matrix.add(flightLine);
+            }
+        }
+
+        for(Pairing p : pairingsList){
+            costMatrix.add(p.getCost());
+        }
+
+        System.out.println("GERADO");
+
+        Simplex exampleProblem = new Simplex(costMatrix, matrix);
+
+        exampleProblem.solve();
+
+        /*
         ArrayList<Double> costMatrix = new ArrayList<Double>(Arrays.asList(30.0, 40.0, 20.0, 10.0, 25.0));
 
         ArrayList<ArrayList<Double>> problemMatrix = new ArrayList<ArrayList<Double>>(Arrays.asList(
@@ -105,6 +151,16 @@ public class Main {
 
         exampleProblem.solve();
 
+        */
+
+    }
+
+    public static boolean hasFlight(Duty d){
+        for(int j = 0; j < d.getFlights().size(); j++){
+            if(d.getFlights().get(j).getFlightId() == 0)
+                return true;
+        }
+        return false;
     }
 
 }
